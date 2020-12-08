7bd52cc10fcf4b751835013ee0f8f020d51d6f7c
Jiri Koutny <jirkakoutny@gmail.com>
2020-12-08 09:58:03 +0100

    Poetry updated to 1.1.4

--- src/penvy/PenvyConfig.py
+++ src/penvy/PenvyConfig.py
@@ -16,7 +16,7 @@ class PenvyConfig(EnvConfig):
             "logger": {"name": "env-init"},
             "conda": {"minimal_version": "4.7.12"},
             "git": {"minimal_version": "2.24.0"},
-            "poetry": {"install_version": "1.0.0"},
+            "poetry": {"install_version": "1.1.4"},
         }
 
     def get_parameters_resolvers(self, default_config: dict) -> List[ParametersResolverInterface]:
