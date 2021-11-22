7c05675a0a31d9cd87f4f7095fa335971952f964
Karel Gudera <karel.gudera@datasentics.com>
2021-11-22 08:30:59 +0100

    Raise exception if process with live output fails

--- src/penvy/shell/runner.py
+++ src/penvy/shell/runner.py
@@ -18,12 +18,17 @@ def run_shell_command(command: str, cwd: str = os.getcwd(), shell=False):
     proc = subprocess.Popen(command, cwd=cwd, shell=shell)
     proc.communicate()
 
-    if proc.returncode > 0:
+    if proc.returncode != 0:
         raise Exception(f"Shell command failed with code: {proc.returncode}")
 
 
 def run_with_live_output(command: str, cwd: str = os.getcwd(), shell=False):
-    process = subprocess.Popen(command, cwd=cwd, shell=shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = subprocess.Popen(command, cwd=cwd, shell=shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    for line in iter(process.stdout.readline, b""):
+    for line in iter(proc.stdout.readline, b""):
         sys.stdout.write(line)
+
+    proc.communicate()
+
+    if proc.returncode != 0:
+        raise Exception(f"Shell command failed with code: {proc.returncode}")
